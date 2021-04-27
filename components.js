068af4a31176abf50ebe7f0a6878981680879272
Abbey Sparrow <abbey@khrome.net>
2021-04-27 11:17:13 -0600

    updates

--- components.js
+++ components.js
@@ -12,38 +12,50 @@ const AsciiArtHeader = {
   font: property(''),
   text: property(''),
   renderedText: ({ text }) => {
-
+      return text;
   },
-  render: ({ renderedText }) => html`
+  render: ({ renderedText }) => {
+      var promise = new Promise((resolve, reject) => {
+          AsciiArt.font(renderedText, 'u:doublestrike', function(err, rendered){
+              if(err) return reject(err);
+              resolve(rendered);
+          });
+      });
+      return html`
       <style>
           div.ascii-art-header{
 
           }
       </style>
-      <span class="ascii-art-header">${renderedText}</span>
-  `,
+      <span class="ascii-art-header">${html.resolve(
+      promise
+        .then((value) => html`<div>${value}</div>`)
+        .catch(() => html`<div>Error!</div>`),
+      html`Loading...`,
+    )}</span>
+  `},
 };
 
 define('ascii-art-header', AsciiArtHeader);
 
 function imageLoad(host, event){
-    console.log('LOADED', host, event)
     var img = event.target;
     Art.image({
         imageBody : img,
         alphabet: 'variant1',
-        height : 32,
-        width : 32,
+        height : host.rows,
+        width : host.cols,
+        stippled : host.stipple,
+        threshold : host.threshold,
+        posterized : host.posterize,
         imageInfo : {
             height : img.naturalHeight,
             width : img.naturalWidth,
         }
     }, function(err, rendered){
         if(err) throw err;
-        console.log('STARTED');
         setTimeout(function(){
             var html = window.ansi2html?window.ansi2html(rendered):rendered;
-            console.log('RENDERED', host, html);
             host.renderedSrc = html;
         }, 100)
     });
@@ -54,6 +66,11 @@ const AsciiArtImage = {
     src: property(''),
     renderedSrc: property(''),
     imageTxt: property(''),
+    rows: property('32'),
+    cols: property('32'),
+    threshold: property(128),
+    posterize: property(false),
+    stipple: property(''),
     htmlTxt: ({ imageTxt }) => {
         return imageTxt;
     },
@@ -61,7 +78,6 @@ const AsciiArtImage = {
         <style>
             img.ascii-art-image{
                 ${(renderedSrc?'display:none; ':'')}
-                font-family: "Courier New", Courier, monospace;
             }
         </style>
         ${(
@@ -69,12 +85,12 @@ const AsciiArtImage = {
             html`
                 <div
                     class="ascii-art-image"
-                    style="font-family: Courier, monospace"
+                    style="font-family: Courier, monospace; line-height:1;"
                     innerHTML="${renderedSrc}"
                 ></div>
             `
         )}
-        <img class="ascii-art-image" onload="${imageLoad}" src="${src}"/>
+        <img class="ascii-art-image" onload="${imageLoad}" src="${src}">
     `,
 };
 
